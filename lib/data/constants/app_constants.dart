84ee0aa90ca7cdd1a91a3c8a212cd9063ef432b4
Karthicbz <pkarthicbz@live.in>
2026-01-22 06:38:47 +0530

    create strings for login page

---
+++ lib/data/constants/app_constants.dart
@@ -0,0 +1,3 @@
+enum AppLanguage{
+  english, tamil;
+}
---
+++ lib/data/constants/strings.dart
@@ -0,0 +1,7 @@
+final String signInText = "Sign in";
+final String appMessage = "Track your expense offline and sync securely";
+final String emailText = "Email";
+final String fourDigitPinText = "Digit PIN";
+final String rememberMeText = "Remember me";
+final String loginText = "Login";
+final String syncMessage = "Offline mode active. Data will sync automatically";
