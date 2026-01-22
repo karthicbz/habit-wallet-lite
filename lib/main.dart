1bfb3faba7de42c2d8bd1de55e63e27aac51e909
Karthicbz <pkarthicbz@live.in>
2026-01-22 08:03:58 +0530

    change color scheme to green

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.greenAccent),
       ),
       home:  LoginPage(),
     );
