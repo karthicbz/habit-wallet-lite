d3baf4cbe0537531d3802eef42ef19531eb727b0
Karthicbz <pkarthicbz@live.in>
2026-01-23 23:57:35 +0530

    create model for transaction

---
+++ lib/data/models/transaction_model.dart
@@ -0,0 +1,51 @@
+import 'dart:nativewrappers/_internal/vm/lib/typed_data_patch.dart';
+
+import 'package:hive_ce_flutter/adapters.dart';
+
+enum Transaction { income, expense }
+
+enum Category {
+  foodAndGroceries,
+  transport,
+  shopping,
+  utilities,
+  medicine,
+  education,
+  others,
+}
+
+class TransactionModel extends HiveObject {
+  final Transaction transaction;
+  final Category category;
+  final double amount;
+  final DateTime date;
+  final String notes;
+  final List<Uint8List> files;
+
+  TransactionModel({
+    required this.transaction,
+    required this.category,
+    required this.amount,
+    required this.date,
+    required this.notes,
+    required this.files,
+  });
+
+  TransactionModel copyWith({
+    Transaction? transaction,
+    Category? category,
+    double? amount,
+    DateTime? date,
+    String? notes,
+    List<Uint8List>? files,
+  }) {
+    return TransactionModel(
+      transaction: transaction ?? this.transaction,
+      category: category ?? this.category,
+      amount: amount ?? this.amount,
+      date: date ?? this.date,
+      notes: notes ?? this.notes,
+      files: files ?? this.files,
+    );
+  }
+}
