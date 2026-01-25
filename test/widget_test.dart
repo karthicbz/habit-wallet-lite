925e6896493cd6f77ebf686a6684c75d6ddfb1e9
Karthicbz <pkarthicbz@live.in>
2026-01-25 08:24:10 +0530

    fix test code which is crashing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,27 +7,77 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:habit_wallet_lite/data/constants/app_constants.dart';
+import 'package:habit_wallet_lite/data/models/app_locale_model.dart';
+import 'package:habit_wallet_lite/data/models/settings_model.dart';
+import 'package:habit_wallet_lite/data/models/transaction_category_model.dart';
+import 'package:habit_wallet_lite/data/models/transaction_model.dart';
+import 'package:habit_wallet_lite/data/providers/locale_provider.dart';
+import 'package:habit_wallet_lite/data/providers/settings_provider.dart';
 import 'package:habit_wallet_lite/data/providers/transaction_category_provider.dart';
 import 'package:habit_wallet_lite/data/providers/transaction_list_provider.dart';
 
 import 'package:habit_wallet_lite/main.dart';
 import 'package:habit_wallet_lite/views/pages/login_page.dart';
 
+// Mock Notifiers
+class MockSettingsNotifier extends SettingsNotifier {
+  @override
+  SettingsModel build() {
+    return SettingsModel(
+      darkMode: false,
+      language: Language.english,
+      remainder: false,
+      autoLogin: false,
+    );
+  }
+}
+
+class MockLocaleNotifier extends LocaleNotifier {
+  @override
+  AppLocaleModel build() {
+    return AppLocaleModel(appLocale: AppLocale.en);
+  }
+}
+
+class MockTransactionListNotifier extends TransactionListNotifier {
+  @override
+  TransactionListHelper build() {
+    return TransactionListHelper(
+      transactionModel: [],
+      isLoading: false,
+      dummyTransactionModel: [],
+    );
+  }
+}
+
+class MockTransactionCategoryNotifier extends TransactionCategoryNotifier {
+  @override
+  List<TransactionCategoryModel> build() {
+    // Return a basic list of categories with 0 spent/limit
+    return Category.values
+        .map(
+          (c) => TransactionCategoryModel(category: c, limit: 0.0, spent: 0.0),
+        )
+        .toList();
+  }
+}
+
 void main() {
   testWidgets('App launches without crashing', (WidgetTester tester) async {
     await tester.pumpWidget(
       ProviderScope(
         overrides: [
+          settingsProvider.overrideWith(() => MockSettingsNotifier()),
+          localeProvider.overrideWith(() => MockLocaleNotifier()),
           transactionListProvider.overrideWith(
-                () => TransactionListNotifier(),
+            () => MockTransactionListNotifier(),
           ),
           transactionCategoryProvider.overrideWith(
-                () => TransactionCategoryNotifier(),
+            () => MockTransactionCategoryNotifier(),
           ),
         ],
-        child: const MyApp(
-          initialHome: LoginPage(),
-        ),
+        child: const MyApp(initialHome: LoginPage()),
       ),
     );
 
